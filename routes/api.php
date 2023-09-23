e7cd72b0492ac10f23edf3d7d743a2c7035ea79f
samirraihan <samirraihan93@gmail.com>
2023-09-23 14:40:08 +0600

    API version added

--- routes/api.php
+++ routes/api.php
@@ -12,25 +12,27 @@ Route::get('/sanctum/csrf-cookie', function (Request $request) {
     return response()->json(['csrf' => csrf_token()]);
 });
 
-// Public routes
-Route::group(['prefix' => 'auth'], function () {
-    Route::post('/signin', [AuthController::class, 'signin']);
-    Route::post('/signup', [AuthController::class, 'signup']);
-    Route::get('/verify', [AuthController::class, 'verifyEmail'])->name('verification.verify');
-    Route::post('/resendVerificationEmail', [AuthController::class, 'resendVerificationEmail']);
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
+Route::prefix('v1')->group(function () {
+    // Public routes
+    Route::group(['prefix' => 'auth'], function () {
+        Route::post('/signin', [AuthController::class, 'signin']);
+        Route::post('/signup', [AuthController::class, 'signup']);
+        Route::get('/verify', [AuthController::class, 'verifyEmail'])->name('verification.verify');
+        Route::post('/resendVerificationEmail', [AuthController::class, 'resendVerificationEmail']);
+        Route::post('/logout', [AuthController::class, 'logout']);
+    });
 
-// Protected routes
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::group(['prefix' => 'admin'], function () {
-        Route::group(['prefix' => 'tasks'], function () {
-            Route::get('/', [TaskController::class, 'index']);
-            Route::get('/{id}/edit', [TaskController::class, 'edit']);
-            Route::post('/store', [TaskController::class, 'store']);
-            Route::post('/{id}/update', [TaskController::class, 'update']);
-            Route::post('/{id}/delete', [TaskController::class, 'delete']);
-            Route::post('/{id}/comment', [TaskController::class, 'comment']);
+    // Protected routes
+    Route::group(['middleware' => ['auth:sanctum']], function () {
+        Route::group(['prefix' => 'admin'], function () {
+            Route::group(['prefix' => 'tasks'], function () {
+                Route::get('/', [TaskController::class, 'index']);
+                Route::get('/{id}/edit', [TaskController::class, 'edit']);
+                Route::post('/store', [TaskController::class, 'store']);
+                Route::post('/{id}/update', [TaskController::class, 'update']);
+                Route::post('/{id}/delete', [TaskController::class, 'delete']);
+                Route::post('/{id}/comment', [TaskController::class, 'comment']);
+            });
         });
     });
 });
